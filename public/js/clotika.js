490540e3f8958c32d14f9e9ad4dde9a7b2cd58a2
Shlomit Cohen <shlomit.cohen@supersonic.com>
2016-05-17 23:12:03 +0300

    UI route

---
+++ public/js/clotika.js
@@ -0,0 +1,24 @@
+/**
+ * Created by shlomit.c on 17/05/2016.
+ */
+(function() {
+
+    var Clotika = angular.module('Clotika', [
+        'ui.router'
+    ]);
+
+    Clotika.config(function($urlRouterProvider, $stateProvider){
+        $stateProvider
+            .state('home', {
+                url: '/',
+                template:'<div>home</div>'
+                //templateUrl: ''
+            })
+            .state('login', {
+                url: '/login',
+                templateUrl: 'Templates/login.html'
+            })
+        ;
+    })
+
+})
